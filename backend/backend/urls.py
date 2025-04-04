afc3cea2fe03c25ee84387e5b913d5c4346ba27e
Md Atif Husain <mdatifhusain4@gmail.com>
2025-04-04 19:33:06 +0530

    Setup Djoser

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('todo.urls')),
+    path("api/v1/auth/", include('djoser.urls')),
+    path("api/v1/auth/", include('djoser.urls.jwt')),
 ]
