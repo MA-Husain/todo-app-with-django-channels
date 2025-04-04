4d09cbeddd2cf41b8791b56b5b613919b1044ef1
Md Atif Husain <mdatifhusain4@gmail.com>
2025-04-04 23:08:55 +0530

    Setup Backend Authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,3 +177,13 @@ DJOSER = {
         'user_delete': "djoser.serializers.UserDeleteSerializer",      
     },
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@todoapp.com"  # Update email for authentication purposes
+DOMAIN = env("DOMAIN")  # Replace with environment variable for your app's domain
+SITE_NAME = "ToDoApp"  # Name of your application
