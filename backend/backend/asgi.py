24d2cd54dfd23720c1f1f1942099d0411484f769
Md Atif Husain <mdatifhusain4@gmail.com>
2025-04-17 15:52:10 +0530

    Configured asgi

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,7 +1,10 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings') 
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
-from todo.middleware import JWTAuthMiddleware  # 👈
+from todo.middleware import JWTAuthMiddleware
 from todo.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
